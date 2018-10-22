250ad046c7ea32cba39ceb6056d2b832353ff6e4
Michael Samuel <samfam921@gmail.com>
2018-10-21 17:06:22 -0700

    initial commit

---
+++ Color Resources/colorSquareNAME.R
@@ -0,0 +1,30 @@
+#par(mar=c(1,1,2,1))
+
+stopifnot(length(colors()) == 657)
+
+SetTextContrastColor <- function(color)
+{
+  ifelse( mean(col2rgb(color)) > 127, "black", "white")
+}
+TextContrastColor <- unlist( lapply(colors(), SetTextContrastColor) )
+colCount <- 10  # number per row
+rowCount <- 67
+
+plot( c(1,colCount), c(0,rowCount), type="n", ylab="", xlab="",
+      axes=FALSE, ylim=c(rowCount,0))
+title("R colors")
+mtext("http://research.stowers-institute.org/efg/R/Color/Chart",
+      cex=0.6)
+
+for (j in 0:(rowCount-1))
+{
+  base <- j*colCount
+  remaining <- length(colors()) - base
+  RowSize <- ifelse(remaining < colCount, remaining, colCount)
+  rect((1:RowSize)-0.5,j-0.5, (1:RowSize)+0.5,j+0.5,
+       border="black",
+       col=colors()[base + (1:RowSize)])
+  text((1:RowSize), j, colors()[base + (1:RowSize)], cex=0.5,
+       col=TextContrastColor[base + (1:RowSize)])
+}
+
